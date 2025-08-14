a72b8a1f4fd3f31f6157b3b949e2edb662bf5999
thisalma <alwisthisalma@gamil/.com>
2025-08-14 18:17:08 +0530

    making errors

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -14,6 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
         context,
         MaterialPageRoute(builder: (context) => const HomePage()),
       );
