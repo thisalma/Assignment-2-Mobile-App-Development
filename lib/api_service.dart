dc24df3376ec6f4d884d12391a336c5604de5865
thisalma <alwisthisalma@gamil/.com>
2025-09-14 12:34:21 +0530

    adding the token part

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -3,66 +3,46 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = "http://10.0.2.2:8000/api"; // for Android Emulator
-  // For iOS Simulator use: http://127.0.0.1:8000/api
-  // For physical devices, use your machine's IP
+  static const String baseUrl = "http://192.168.1.6:8000/api"; // use your PC IP
 
-  /// Register user
-  static Future<Map<String, dynamic>> register(
-      String name, String email, String password) async {
-    final url = Uri.parse("$baseUrl/register");
+  Future<bool> register(String username, String email, String password) async {
     final response = await http.post(
-      url,
+      Uri.parse('$baseUrl/register'),
       headers: {"Content-Type": "application/json"},
       body: jsonEncode({
-        "name": name,
-        "email": email,
-        "password": password,
-        "password_confirmation": password,
-      }),
-    );
-
-    return _handleResponse(response);
-  }
-
-  /// Login user
-  static Future<Map<String, dynamic>> login(
-      String email, String password) async {
-    final url = Uri.parse("$baseUrl/login");
-    final response = await http.post(
-      url,
-      headers: {"Content-Type": "application/json"},
-      body: jsonEncode({
+        "username": username,
         "email": email,
         "password": password,
       }),
     );
 
-    return _handleResponse(response);
-  }
-
-  /// Save token locally
-  static Future<void> saveToken(String token) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString("token", token);
-  }
-
-  /// Logout user
-  static Future<void> logout() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove("token");
-  }
-
-  /// Response handler
-  static Map<String, dynamic> _handleResponse(http.Response response) {
-    final data = jsonDecode(response.body);
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      return {"success": true, "data": data};
+    if (response.statusCode == 201) {
+      final data = jsonDecode(response.body);
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', data['token']);
+      return true;
     } else {
-      return {
-        "success": false,
-        "message": data["message"] ?? "Something went wrong"
-      };
+      return false;
+    }
+  }
+
+  Future<bool> login(String username, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/login'),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "username": username,
+        "password": password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', data['token']);
+      return true;
+    } else {
+      return false;
     }
   }
 }
